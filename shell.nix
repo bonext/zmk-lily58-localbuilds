23dac0c29999e43cd99ebb9a3f00e338a7e3ba3c
Alexander Molchanov <bonext@mailbox.org>
2023-10-26 10:42:02 +0200

    add zephyr sdk

--- shell.nix
+++ shell.nix
@@ -1,10 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [
+    packages = with pkgs; [
       cmake
       dtc
       python311
       python311Packages.west
     ];
-}
+
+    PROJECT_ROOT = builtins.toString ./.;
+
+    shellHook = ''
+      source $PROJECT_ROOT/zephyr-sdk-0.15.0/environment-setup-x86_64-pokysdk-linux
+    '';
+  }
